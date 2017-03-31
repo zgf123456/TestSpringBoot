555f8cf38dc50d959d6bfd2832ff356836938309
zgf <guofengzheng1@creditease.cn>
2017-03-31 11:20:29 +0800

    test

---
+++ src/main/java/com/zgf/springboot/TestSpringBoot.java
@@ -0,0 +1,10 @@
+package com.zgf.springboot;
+
+/**
+ * Created by zgf on 17/3/31.
+ */
+public class TestSpringBoot {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
