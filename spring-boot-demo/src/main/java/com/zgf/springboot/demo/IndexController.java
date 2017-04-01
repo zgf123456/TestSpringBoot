662710ab0d7b7173b2ba9bcb067f3c6f1bf4b181
zgf <guofengzheng1@creditease.cn>
2017-04-01 12:12:37 +0800

    demo

--- spring-boot-demo/src/main/java/com/zgf/springboot/demo/IndexController.java
+++ spring-boot-demo/src/main/java/com/zgf/springboot/demo/IndexController.java
@@ -11,15 +11,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 @EnableAutoConfiguration
-public class SampleController {
+public class IndexController {
 
     @RequestMapping("/")
     @ResponseBody
-    String home() {
+    public String home() {
         return "Hello World!";
     }
 
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(SampleController.class, args);
+        SpringApplication.run(IndexController.class, args);
     }
 }
